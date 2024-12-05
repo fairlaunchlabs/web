295a5f0ce41a9151683038e794db4bcd8abe5795
jackygu2006 <754800643@qq.com>
2024-12-06 06:22:29 +0800

    update token chart component

--- src/components/tokenDetails/TokenCharts.tsx
+++ src/components/tokenDetails/TokenCharts.tsx
@@ -560,12 +560,16 @@ export const TokenCharts: React.FC<TokenChartsProps> = ({ token }) => {
                             onClick={() => setIsLineChart(!isLineChart)}
                         >
                             {isLineChart ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12H4V4z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                    <path d="M3 3v18h18" strokeLinecap="round" strokeLinejoin="round"/>
+                                    <path d="M7 8h2v8H7z"/>
+                                    <path d="M11 6h2v10h-2z"/>
+                                    <path d="M15 10h2v6h-2z"/>
                                 </svg>
                             ) : (
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                                    <path d="M3 3v18h18" strokeLinecap="round" strokeLinejoin="round"/>
+                                    <path d="M3 12s4-9 6-9 4 6 6 6 4-9 6-9" strokeLinecap="round" strokeLinejoin="round"/>
                                 </svg>
                             )}
                         </button>
